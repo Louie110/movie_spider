1d0ecde145b64be4fe359e5bb2e5eac0c70cb954
Louie110 <1648212702@qq.com>
2018-11-03 21:24:15 +0800

    add content

--- db.sql
+++ db.sql
@@ -1,57 +1,23 @@
 
--------douban中各字段含义----------
-movie_name      电影名称
-actor            演员
-geners           电影类型
-vote_count       投票数
-rating_count     评分
-----------------------------------
-
-create table douban (
-movie_name varchar(60),
-actor varchar(10),
-geners varchar(50),
-rating_count int(10),
-vote_count int(20)
-) default charset=utf8;
-
----查看表结构
-desc douban；
 
 
-
-
--------tencent中各字段含义----------
-movie_name       电影名称
-actor            演员
-geners           电影类型
-figure_count     播放次数
-score            评分
-----------------------------------
-
-create table tencent (
-movie_name varchar(60),
-actor varchar(10),
-score int(10),
-figure_count int(20)
-) default charset=utf8;
-
----查看表结构
-desc tencent；
-
-
--------IMDB中各字段含义----------
-imdb_name       电影名称
-imdb_time       上映年份
-imdb_type       电影类型
+-------movie中各字段含义----------
+name           电影名称
+director       导演
+origin         所属网站
+movie_type     电影类型
 runtime         时长
-meta_score      投票数
+meta_score      评分
+movie_type      电影类型
+vote_count      投票数
 ----------------------------------
 
-create table IMDB (
-imdb_name varchar(60),
-imdb_time year(10),
-imdb_type varchar(10),
+create table movie (
+name varchar(50),
+director varchar(50),
+origin varchar(20),
 runtime int(10),
-meta_score int(10)
+meta_score int(10),
+movie_type varchar(10),
+vote_count int(10)
 ) default charset=utf8;
--- douban/douban.py
+++ douban/douban.py
@@ -1,1 +1,63 @@
-import bs4
+# _*_ coding: utf-8 _*_
+
+#import related library
+import requests
+from bs4 import BeautifulSoup
+
+#get the content of url
+def get_content(url):
+    try:
+        html = requests.get(url)
+    except Exception:
+        print('url 请求失败')
+    else:
+        return html.content
+
+#parse page,extract the links
+def get_link(content):
+    soup = BeautifulSoup(content,'html.parser')
+    try:
+        result = soup.find_all('div',class_ = 'item')
+    except:
+        print('解析页面错误，请检查')
+    else:
+        pass
+    link_list = []
+    for link in result:
+        bref = link.find('a').get('href')
+        link_list.append(bref)
+    return link_list
+
+#extract the field which we need 
+def get_info(urls):
+    info_list = []
+    for url in urls:
+        info_dic = {}
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content,'html.parser')
+        info_dic['name'] = soup.find('h1').text.strip().split(' ')[0]
+        info_dic['director'] = soup.find('span',class_ = 'attrs').text
+        type_list = soup.find_all('span',property = 'v:genre')
+        ts = []
+        for types in type_list:
+            ts.append(types.text)
+        info_dic ['movie_type'] = ts
+        info_dic['run_time'] = soup.find('span',property = 'v:runtime').text
+        info_dic['meta_score'] = soup.find('strong',class_ = 'll rating_num').text
+        info_dic['vote_count'] = soup.find('span',property = 'v:votes').text
+        if '\n' in info_dic['name']:
+            info_dic['name'].replace('\n','')
+        else:
+            pass
+        info_list.append(info_dic)
+    print(info_list)
+
+#main faction
+def main():
+    url = 'https://movie.douban.com/top250'
+    result =  get_content(url)
+    urls = get_link(result)
+    get_info(urls)
+
+if __name__ == '__main__':
+    main()
